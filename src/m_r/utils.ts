4ce15bf5cab5edd6bb04c10d69e544cd7ad70581
Justin Lee <yuminjustin@163.com>
2023-02-07 16:19:07 +0800

    Create utils.ts

---
+++ src/m_r/utils.ts
@@ -0,0 +1,23 @@
+export const getImageSize = (url: string | undefined): Promise<itemSize> => {
+  return new Promise((resolve, reject) => {
+    if (url) {
+      let image = new Image();
+      image.onload = () =>
+        resolve({
+          width: image.width,
+          height: image.height,
+        });
+      image.onerror = () => reject(new Error("error"));
+      image.src = url;
+    } else reject(new Error("url is empty"));
+  });
+};
+
+export const listMaker = (list: Array<mrObject>): Array<mrObject2> => {
+  return list.map((item: mrObject, key: number) => {
+    return Object.assign(
+      { index: key, zIndex: key + 1, left: 0, top: 0, width: 0, height: 0 },
+      item
+    );
+  });
+};
