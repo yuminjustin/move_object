e5ac6c5ccee886b6f7e31a4fd611c17b155abc67
Justin Lee <yuminjustin@163.com>
2023-02-07 16:16:40 +0800

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+interface mrObject {
+  type: string;
+  src?: string;
+}
+interface mrObject2 extends mrObject {
+  index: number;
+  zIndex: number;
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+interface itemSize {
+  width: number;
+  height: number;
+}
+interface itemPosition {
+  top: number;
+  left: number;
+}
+interface Prop {
+  list: Array<mrObject>;
+  style: object;
+  width: number;
+  height: number;
+}
